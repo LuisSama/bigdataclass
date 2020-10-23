6643fb35d92bacffd31ad1a8534675e2417b7c88
luisrra17 <luisrra17>
2020-10-22 20:42:17 -0600

    Joiner

--- testsample/joiner.py
+++ testsample/joiner.py
@@ -2,6 +2,8 @@ from pyspark.sql.functions import col
 
 """Toy join function to showcase spark functions."""
 
-def join_dataframes(left, right, columns_left, columns_right):
-    cond = [col(left_col) == col(right_col) for (left_col, right_col) in zip(columns_left, columns_right)]
-    return left.join(right, cond)
+def join_dataframes(left, right, columns_left, columns_right, join_type='inner'):
+    if len(columns_left) == len(columns_right) and columns_left > 0:
+        cond = [col(left_col) == col(right_col) for (left_col, right_col) in zip(columns_left, columns_right)]
+        return left.join(right, cond, join_type)
+    raise Error('Columns parameters don\'t match or empty')
