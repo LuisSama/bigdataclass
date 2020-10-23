84299c236ad97b016365d51679324af24475ac6c
luisrra17 <luisrra17>
2020-10-22 20:34:45 -0600

    Update joiner.py

--- testsample/joiner.py
+++ testsample/joiner.py
@@ -1,4 +1,7 @@
+from pyspark.sql.functions import col
+
 """Toy join function to showcase spark functions."""
 
 def join_dataframes(left, right, columns_left, columns_right):
-    pass
+    cond = [col(left_col) == col(right_col) for (left_col, right_col) in zip(columns_left, columns_right)]
+    return left.join(right, cond)
